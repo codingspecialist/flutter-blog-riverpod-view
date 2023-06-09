eb737622ade0ea27e82dcfc454f9b08c5da0167b
nomadhuns <kimalss22@gmail.com>
2023-06-09 11:10:44 +0900

    로그인 통신(Repository) 메소드 구현
    - UserRepository fetchLogin 메소드 구현
    - LoginReqDTO 클래스 생성
    - 공통 응답 DTO를 담을 클래스 생성

--- lib/model/user/user_repository.dart
+++ lib/model/user/user_repository.dart
@@ -1,3 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_blog_2/core/constants/http.dart';
+import 'package:flutter_blog_2/dto/response_dto.dart';
+import 'package:flutter_blog_2/dto/user_request.dart';
+import 'package:flutter_blog_2/model/user/user.dart';
+
 class UserRepository {
   static final UserRepository _instance = UserRepository._single();
   factory UserRepository() {
@@ -5,4 +11,23 @@ class UserRepository {
   }
   UserRepository._single();
 
+  Future<ResponseDTO> fetchLogin(LoginReqDTO loginReqDTO) async {
+    try{
+      // 1. 통신 시작
+      Response response = await dio.post("/login", data: loginReqDTO.toJson());
+
+      // 2. DTO 파싱
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+      responseDTO.data = User.fromJson(responseDTO.data);
+
+      // 3. 토큰 받기
+      final authorization = response.headers["authorization"];
+      if(authorization != null){
+        responseDTO.token = authorization.first;
+      }
+      return responseDTO;
+    }catch(e){
+      return ResponseDTO(code: -1, msg: "유저네임 혹은 비번이 틀렸습니다");
+    }
+  }
 }
